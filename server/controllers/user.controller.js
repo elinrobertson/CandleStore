00ebd11529e476787a9a42dcd79b784ca5350552
Elin Robertson <robertsonelin@gmail.com>
2023-09-08 13:47:30 +0200

    fixed hashed password

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 async function register(req, res) {
     try {
-        const { name, email, password } = req.body;
+        const { name, email } = req.body;
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); // Kryptera lösenordet med bcrypt
 
         // Kolla om användaren redan finns i Stripe (baserat på e-postadress eller annan unik identifierare)
         const existingCustomer = await stripe.customers.list({ email: email });
@@ -16,6 +19,7 @@ async function register(req, res) {
         const customer = await stripe.customers.create({
             name: name,
             email: email,
+            // password: hashedPassword
         });
 
         const filePath = "./db/customers.json";
@@ -34,7 +38,8 @@ async function register(req, res) {
             const newUser = {
             name: name,
             email: email,
-            stripeCustomerId: customer.id
+            password: hashedPassword,
+            stripeCustomerId: customer.id,
         };
 
         existingUsers.push(newUser);
@@ -53,54 +58,6 @@ async function register(req, res) {
         res.status(400).json(error.message);
     }
 }
-
-// async function register (req, res) {
-//     try {
-//         const { name, email } = req.body;
-
-//         const customer = await stripe.customers.create({
-//             name: name,
-//             email: email,
-//         });
-        
-//         const filePath = "./db/customers.json";
-
-//         let existingUsers = [];
-
-//         if(fs.existsSync(filePath)) {
-//             const fileData = fs.readFileSync(filePath, "utf8");
-//             existingUsers = JSON.parse(fileData);
-//         }
-
-//         if (!Array.isArray(existingUsers)) {
-//             existingUsers = [];
-//           }
-
-//         const newUser = {
-//             name: "Amanda",
-//             email: "amanda@email.com",
-//             stripeCustomerId: customer.id,
-//         };
-
-//         existingUsers.push(newUser);
-
-//         const jsonData = JSON.stringify(existingUsers, null, 2);
-
-//         fs.writeFileSync(filePath, jsonData)
-
-
-//     res.status(200).json({
-//         message: "Användare skapad och tillagd",
-//         user: newUser,
-//     });
-//     } catch (error) {
-//         console.log("Fel vid registrering" , error);
-//         res.status(500).json({ error: "Ett fel uppstod vid registrering." })
-//     }
-// }
-
-
-
 
 
 
