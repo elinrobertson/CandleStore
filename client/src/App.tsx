3f77485fd69a13835f36d23f0f43618a69c67016
Elin Robertson <robertsonelin@gmail.com>
2023-09-11 14:37:11 +0200

    started setting up routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/Home";
-import Confirmation from "./components/Confirmation";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+
+// import Confirmation from "./components/Confirmation";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/confirmation" element={<Confirmation />}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Home />
+      <Header />
+    </div>
+
   )
 }
 
